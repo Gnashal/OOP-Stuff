3cbe63b9b2440bbe2884361ece5a400c98660d89
Gnashal <yousifceballos@gmail.com>
2024-11-13 15:03:35 +0800

    just copied stuff here from the pc

---
+++ Employee_Series/empseries/src/main/java/com/employeeseries/version7/Employee.java
@@ -0,0 +1,80 @@
+package com.employeeseries.version6;
+
+public abstract class Employee {
+    public Name empName;
+    public MyDate dateHired;
+    public MyDate birthDate;
+    public int id;
+
+    public Employee() {
+
+    }
+    public Employee(Name name, MyDate hire,MyDate dob) {
+        this.empName = new Name(name.getLname(), name.getFname(), name.getMname());
+        this.dateHired = new MyDate(hire.getDay(), hire.getMonth(), hire.getYear());
+        this.birthDate = new MyDate(dob.getDay(), dob.getMonth(), dob.getYear());
+     }
+     public Employee(String fname, String lname, MyDate hire,MyDate dob) {
+        this.empName = new Name(lname, fname);
+        this.dateHired = new MyDate(hire.getDay(), hire.getMonth(), hire.getYear());
+        this.birthDate = new MyDate(dob.getDay(), dob.getMonth(), dob.getYear());
+     }
+
+    public Employee(Name name, MyDate hire,MyDate dob, int id) {
+       this.empName = new Name(name.getLname(), name.getFname(), name.getMname());
+       this.dateHired = new MyDate(hire.getDay(), hire.getMonth(), hire.getYear());
+       this.birthDate = new MyDate(dob.getDay(), dob.getMonth(), dob.getYear());
+       this.id = id;
+    }
+    public Employee(Name name, int id) {
+        this.empName = new Name(name.getLname(), name.getFname(), name.getMname());
+        this.id = id;
+    }
+
+    public void setBirthDate(MyDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    public void setDateHired(MyDate dateHired) {
+        this.dateHired = dateHired;
+    }
+    public void setEmpName(Name empName) {
+        this.empName = empName;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public MyDate getBirthDate() {
+        return birthDate;
+    }
+    public MyDate getDateHired() {
+        return dateHired;
+    }
+    public Name getEmpName() {
+        return empName;
+    }
+    public int getId() {
+        return id;
+    }
+    public Employee getFullEmployee() {
+        return this;
+    }
+
+    public void display(){
+        empName.display();
+        dateHired.display();
+        birthDate.display();
+        System.out.println(id);
+        System.out.println();
+    }
+
+    public abstract double computeSalary();
+
+    @Override
+    public String toString() {
+        return "Name: " + empName.fname + " " + empName.mname + " " + empName.lname;
+    }
+
+
+
+}
+
---
+++ Employee_Series/empseries/src/main/java/com/employeeseries/version7/Hourly.java
@@ -0,0 +1,88 @@
+package com.employeeseries.version6;
+
+
+public class Hourly extends Employee{
+    private float hoursWorked;
+    private double hourlyRate;
+
+    public Hourly() {
+
+    }
+    public Hourly(Employee emp) {
+        super(emp.empName.getFullName(), emp.getDateHired(), emp.getBirthDate(), emp.getId());
+    }
+
+    public Hourly(Name name, MyDate hire, MyDate dob) {
+        super(name.getFullName(), hire.getFullDate(),dob.getFullDate());
+    }
+
+    public Hourly(Name name, int id) {
+        super(name.getFullName(), id);
+    }
+
+    public Hourly(float hoursWorked, double hourlyRate, Employee emp) {
+        super(emp.empName.getFullName(), emp.getDateHired(), emp.getBirthDate(), emp.getId());
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+
+    public Hourly(float hoursWorked, double hourlyRate, Name name, int id) {
+        super(name.getFullName(), id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+    public Hourly(Name name, MyDate hire, MyDate dob, int id, float hourlyRate, float hoursWorked) {
+        super(name, id);
+        super.setBirthDate(dob);
+        super.setDateHired(hire);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+    
+    
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public void setHoursWorked(float hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double computeSalary() {
+        double hw = hoursWorked;
+        double salary = 0;
+        double bonus = 1.5;
+        if (hw > 40) {
+            hw -= 40;
+            hw *= bonus;
+            salary = (hoursWorked * hw) * hoursWorked;
+        } else {
+            salary = (hoursWorked * hourlyRate);
+        }
+
+        return salary;
+    }
+    @Override
+    public void display() {
+        System.out.println("Hourly Employee: ");
+        super.display();
+        System.out.println("Computed Salary: " + computeSalary() + "\n");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n"+ "Hours Worked: " + hoursWorked + "\n"
+        + "Hourly Rate: " + hourlyRate + "\n";
+    }
+}
